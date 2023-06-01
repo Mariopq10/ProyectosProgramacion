b30d0a823fe9e1988ef8443a8a4cabcfc3cacdcc
Mariopq10 <>
2023-06-01 17:02:10 +0200

    hgg

---
+++ examen.final.mpq/src/main/java/interfaces/Ventana.java
@@ -0,0 +1,28 @@
+package interfaces;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+
+	public Ventana() {
+		this.setSize(600, 600);
+		this.setTitle("Examen de Programacion 1 DAW");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setContentPane(new PanelPrincipal(this));
+		this.setLocationRelativeTo(null);
+		this.setResizable(true);
+		this.setVisible(true);
+	}
+
+	public void cambiarAPantalla(Class<?> clase) {
+		this.getContentPane().setVisible(false);
+		if (clase.equals(PanelPrincipal.class)) {
+			this.setContentPane(new PanelPrincipal(this));
+		}
+	}
+}
